dc91315caa5e5e98d9afe529a444f5537e8d6b87
extjpo <ext.jakub.pruszydlo@rail.bombardier.com>
2020-04-01 11:03:05 +0200

    Zmiana

--- CoffeeMug.Api/Program.cs
+++ CoffeeMug.Api/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 //GitTest
-//GitTest2
 namespace CoffeeMug.Api
 {
     public class Program
